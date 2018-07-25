67a733e10e6d04b90af2d275ea8aec8bca849bf7
nomuyoshi <nomuyoshi0111@gmail.com>
2018-07-25 14:54:37 +0900

    現在開いているタブのURLを取得
    変数名をurlに変更

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+/*global chrome*/
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+    };
+  }
+
+  componentDidMount() {
+    chrome.tabs.query({active: true}, (tabs) => {
+      const tab = tabs[0];
+      this.setState({url: tab.url});
+    });
+  }
+
   render() {
+    const url = this.state.url;
+
     return (
       <div className="App">
+        <input type="text" defaultValue={url} />
       </div>
     );
   }

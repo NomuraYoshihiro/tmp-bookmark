d5e571c7c479756859fd54def74a15e58172c9e2
nomuyoshi <nomuyoshi0111@gmail.com>
2018-07-25 15:12:47 +0900

    ページタイトル取得・表示

--- src/App.js
+++ src/App.js
@@ -6,23 +6,37 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      title: '',
       url: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    chrome.tabs.query({active: true}, (tabs) => {
+    chrome.tabs.query({ active: true }, (tabs) => {
       const tab = tabs[0];
-      this.setState({url: tab.url});
+      this.setState({title: tab.title, url: tab.url});
     });
   }
 
+  handleChange(event) {
+    const title = event.target.value;
+    this.setState({ title });
+  }
+
+  addBookmark() {
+    console.log('add bookmark');
+  }
+
   render() {
-    const url = this.state.url;
+    const { title, url } = this.state;
 
     return (
       <div className="App">
+        <input type="text" value={title} onChange={this.handleChange} />
         <input type="text" defaultValue={url} />
+        <button type="button" onClick={this.addBookmark}>追加</button>
       </div>
     );
   }

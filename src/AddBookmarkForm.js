b44056a199ce7b6e41089eba5700f2bff0dd66a0
nomuyoshi <nomuyoshi0111@gmail.com>
2018-07-30 17:50:55 +0900

    追加フォームをコンポーネントに切り出す

---
+++ src/AddBookmarkForm.js
@@ -0,0 +1,71 @@
+/*global chrome*/
+import React, { Component } from 'react';
+
+class AddBookmarkForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      url: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.add = this.add.bind(this);
+  }
+
+  componentDidMount() {
+    chrome.tabs.query({ active: true }, (tabs) => {
+      const tab = tabs[0];
+      this.setState({title: tab.title, url: tab.url});
+    });
+  }
+
+  handleChange(event) {
+    const title = event.target.value;
+    this.setState({ title });
+  }
+
+  add() {
+    const { title, url } = this.state;
+    this.props.addBookmark(title, url);
+  }
+
+  render() {
+    const { title, url } = this.state;
+    return (
+      <React.Fragment>
+        <div className="field is-horizontal">
+          <div className="field-label is-small">
+            <label className="label">名前</label>
+          </div>
+          <div className="field-body">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input is-small" type="text" value={title} onChange={this.handleChange} />
+              </div>
+              <div className="control">
+                <button className="button is-primary is-small" type="button" onClick={this.add}>
+                  追加
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="field is-horizontal">
+          <div className="field-label is-small">
+            <label className="label">URL</label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control">
+                <input className="input is-small" type="text" defaultValue={url} disabled />
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AddBookmarkForm;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 /*global chrome*/
 import React, { Component } from 'react';
+import AddBookmarkForm from './AddBookmarkForm';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      url: '',
       bookmarks: [],
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.addBookmark = this.addBookmark.bind(this);
   }
 
   componentDidMount() {
-    chrome.tabs.query({ active: true }, (tabs) => {
-      const tab = tabs[0];
-      this.setState({title: tab.title, url: tab.url});
-    });
-
     chrome.storage.sync.get(['tmpBookmarks'], (result) => {
       const bookmarks = result.tmpBookmarks;
       if (bookmarks && bookmarks.length > 0) {
@@ -28,13 +21,8 @@ class App extends Component {
     });
   }
 
-  handleChange(event) {
-    const title = event.target.value;
-    this.setState({ title });
-  }
-
-  addBookmark() {
-    const { title, url, bookmarks } = this.state;
+  addBookmark(title, url) {
+    const bookmarks = this.state.bookmarks;
     const newBookmarks = [...bookmarks, { title, url }];
     chrome.storage.sync.set({ tmpBookmarks: newBookmarks }, () => {
       this.setState({ bookmarks: newBookmarks });
@@ -42,44 +30,16 @@ class App extends Component {
   }
 
   render() {
-    const { title, url, bookmarks } = this.state;
+    const bookmarks = this.state.bookmarks;
     return (
-      <React.Fragment>
-        <div className="field is-horizontal">
-          <div className="field-label is-small">
-            <label className="label">名前</label>
-          </div>
-          <div className="field-body">
-            <div className="field has-addons">
-              <div className="control">
-                <input className="input is-small" type="text" value={title} onChange={this.handleChange} />
-              </div>
-              <div className="control">
-                <button className="button is-primary is-small" type="button" onClick={this.addBookmark}>
-                  追加
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="field is-horizontal">
-          <div className="field-label is-small">
-            <label className="label">URL</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <input className="input is-small" type="text" defaultValue={url} disabled />
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container">
+        <AddBookmarkForm addBookmark={this.addBookmark} />
         <div>
           {bookmarks.map((item) =>
             <p>{item.title}</p>
           )}
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
